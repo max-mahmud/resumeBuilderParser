7ac6d5568e796b80032da19c9ed566172d9fad2f
max-mahmud <120459397+max-mahmud@users.noreply.github.com>
2024-01-11 19:10:27 +0600

    update

---
+++ app/lib/redux/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import resumeSlice from "./resumeSlice";
+import settingsSlice from "./settingSlice";
+
+export const store = configureStore({
+  reducer: { resume: resumeSlice, settings: settingsSlice },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
